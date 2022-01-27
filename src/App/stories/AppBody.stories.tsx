432c7f2facf81e918415a257a6f0da6eb989ad58
Ermakov.Alexey <Ermakov.Alexey@russianpost.ru>
2022-01-28 01:26:06 +0300

    [task-10] fix stories

--- src/App/stories/AppBody.stories.tsx
+++ src/App/stories/AppBody.stories.tsx
@@ -13,14 +13,7 @@ export default {
   title: 'Application/AppBody',
 } as ComponentMeta<typeof AppBody>;
 
-const payload = {
-  gameLevel: '1',
-  gameFieldSize: 4,
-  gameFieldPercentFilled: 10,
-  gameFieldData: [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-};
-
-store.dispatch(appSlice.actions.hydrate(payload));
+store.dispatch(appSlice.actions.rehydrate());
 
 const Template: ComponentStory<typeof AppBody> = (args) => (
   <Provider store={store}>
--- src/App/stories/AppHeader.stories.tsx
+++ src/App/stories/AppHeader.stories.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs } from '@storybook/addon-knobs';
 
 import AppHeader from '@src/App/AppHeader';
+import { store } from '@src/store';
+import { authSlice } from '@src/modules/Auth/authSlice';
 
 export default {
   component: AppHeader,
@@ -11,7 +14,19 @@ export default {
   title: 'Application/AppHeader',
 } as ComponentMeta<typeof AppHeader>;
 
-const Template: ComponentStory<typeof AppHeader> = (args) => <AppHeader {...args} />;
+store.dispatch(
+  authSlice.actions.login({
+    login: 'user',
+    password: '123',
+    token: '123-123-123-123',
+  })
+);
+
+const Template: ComponentStory<typeof AppHeader> = (args) => (
+  <Provider store={store}>
+    <AppHeader {...args} />;
+  </Provider>
+);
 
 export const Static = Template.bind({});
 
