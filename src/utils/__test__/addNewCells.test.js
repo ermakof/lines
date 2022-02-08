ae9b7e2be8631fc285bf88b08aa754c13e1b3c60
Ermakov.Alexey <Ermakov.Alexey@russianpost.ru>
2022-02-08 22:17:53 +0300

    [task-13] update tests

--- src/utils/__test__/addNewCells.test.js
+++ src/utils/__test__/addNewCells.test.js
@@ -1,16 +1,15 @@
 import { getNewCells } from '@src/utils';
-import { GAME_LEVEL_SETTINGS } from '@src/App/initialState';
 
 describe('addNewCells', () => {
   it('do not generate new cells', () => {
     const cells = [1, 1, 1, 1, 1, 1, 1];
     const arr = getNewCells(cells, '3');
-    expect(arr.length).toBe(0);
+    expect(arr).toHaveLength(0);
   });
 
   it('add 5 new cells', () => {
     const cells = [0, 0, 0, 0, 0, 0, 0];
     const arr = getNewCells(cells, '3');
-    expect(arr.length).toBe(GAME_LEVEL_SETTINGS['3'].increase);
+    expect(arr).toHaveLength(5);
   });
 });
