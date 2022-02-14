1ae8be48df6eafe7e8906cc99c041c91acea3d73
Ermakov.Alexey <Ermakov.Alexey@russianpost.ru>
2022-02-14 18:05:54 +0300

    [task-13] add fast-check

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,4 +1,5 @@
 import { reducer as app } from '@src/App/appSlice';
 import { reducer as auth } from '@src/modules/Auth/authSlice';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 
-export const rootReducer: any = { app, auth };
+export const rootReducer: ReducersMapObject = { app, auth };
